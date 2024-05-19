c1bbc9f6ffd92fd594208f7b291c780f8c8d76eb
samgeeksdev <samgeeks7@gmail.com>
2024-05-19 13:40:41 +0330

    ss

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'samgeeksdev/anypay',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3932f553a43b2b9438c73c341ee1eb37be31b28a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'samgeeksdev/anypay' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3932f553a43b2b9438c73c341ee1eb37be31b28a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
